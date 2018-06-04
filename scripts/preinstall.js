9bde29a86325db2311d31a8b8eb143f31d5905d5
Lars Heinemann <lhein.smx@gmail.com>
2018-06-04 20:58:30 +0200

    fixed script problems (#7)
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,13 +1,11 @@
 'use strict';
-const download = require("mvn-artifact-download");
-download('com.github.camel-tooling:camel-lsp-server:1.0.0-SNAPSHOT', './jars/language-server.jar', 'https://oss.sonatype.org/content/repositories/snapshots/');
 
-/**
+var lsp_server_version = "1.0.0-SNAPSHOT";
+
+const download = require("mvn-artifact-download").default;
 const fs = require('fs');
-const download = require('download')
-var url = "https://github.com/camel-tooling/camel-language-server/releases/download/untagged-27364bc5f4ff78ca37a9/camel-lsp-server-1.0.0-SNAPSHOT.jar"
+const path = require('path');
 
-download(url).then(data => {
-    fs.writeFileSync('./jars/language-server.jar', data);
+download('com.github.camel-tooling:camel-lsp-server:' + lsp_server_version, './jars/', 'https://oss.sonatype.org/content/repositories/snapshots/').then((filename)=>{
+	fs.renameSync(filename, path.join('.', 'jars', 'language-server.jar'));
 });
- */
