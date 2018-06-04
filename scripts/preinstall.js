39b9a56a4362e5c4ac4f0679c37c50fdd17521e9
Lars Heinemann <lhein.smx@gmail.com>
2018-06-04 16:18:53 +0200

    download server from central/sonatype (#7)
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,4 +1,8 @@
 'use strict';
+import download from 'mvn-artifact-download';
+download('com.github.camel-tooling:camel-lsp-server:1.0.0-SNAPSHOT', './jars/language-server.jar', 'https://oss.sonatype.org/content/repositories/snapshots/');
+
+/**
 const fs = require('fs');
 const download = require('download')
 var url = "https://github.com/camel-tooling/camel-language-server/releases/download/untagged-27364bc5f4ff78ca37a9/camel-lsp-server-1.0.0-SNAPSHOT.jar"
@@ -6,3 +10,4 @@ var url = "https://github.com/camel-tooling/camel-language-server/releases/downl
 download(url).then(data => {
     fs.writeFileSync('./jars/language-server.jar', data);
 });
+ */
