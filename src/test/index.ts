598e024959fb2b25d96111f1a59f217fd93f38c7
Aurélien Pupier <apupier@redhat.com>
2019-06-28 10:20:07 +0200

    Add Jenkinsfile capable of building, testing and publishing #107
    need to adapt test runner reporter to have it compatible with
    jenkinsfile
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/index.ts
+++ src/test/index.ts
@@ -5,7 +5,8 @@ import * as testRunner from 'vscode/lib/testrunner';
 testRunner.configure({
     ui: 'bdd',
     useColors: true,
-    timeout: 100000
+    timeout: 100000,
+    reporter: 'mocha-jenkins-reporter'
 });
 
 module.exports = testRunner;
