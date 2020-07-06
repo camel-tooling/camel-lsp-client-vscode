9bf56048fe87f5dddbe6e45a62caf919b7a74a78
Aurélien Pupier <apupier@redhat.com>
2020-07-06 17:41:04 +0200

    FUSETOOLS2-470 - provide basic test for modeline completion
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

---
+++ test Fixture with speci@l chars/ModelineCompletion.java
@@ -0,0 +1,17 @@
+// camel-k: 
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class ModelineCompletion extends RouteBuilder {
+
+    public void configure() {
+
+        from("timer:timerName?delay=1000")
+            .choice()
+                .when(xpath("/person/city = 'London'"))
+                    .to("file:target/messages/uk")
+                .otherwise()
+                    .to("file:target/messages/others");
+    }
+
+}
