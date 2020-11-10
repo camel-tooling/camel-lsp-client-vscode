a1fdd646ebc2a3652004e79aa678c2b84000c363
Aurélien Pupier <apupier@redhat.com>
2020-11-10 10:22:38 +0100

    try to use waitUntil for the failing completion test on CircleCI

--- src/test/catalog.version.test.ts
+++ src/test/catalog.version.test.ts
@@ -5,12 +5,11 @@ import * as chai from 'chai';
 import { getDocUri, activate } from './helper';
 
 const expect = chai.expect;
+const waitUntil = require('async-wait-until');
 
 describe('Should do completion in Camel URI using the Camel Catalog version specified in preference', () => {
 	const docUriXml = getDocUri('test-catalog-version.xml');
-	const expectedCompletion = [
-		{ label: 'jgroups-raft:clusterName'}
-	];
+	const expectedCompletion = { label: 'jgroups-raft:clusterName'};
 
 	afterEach(() => {
 		let config = vscode.workspace.getConfiguration();
@@ -18,39 +17,47 @@ describe('Should do completion in Camel URI using the Camel Catalog version spec
 	});
 
 	it('Updated Catalog version is reflected in completion', async () => {
-		await testCompletion(docUriXml, new vscode.Position(0, 21), {
-			items: expectedCompletion
-		});
+		await activate(docUriXml);
 		let config = vscode.workspace.getConfiguration();
+		expect(config.get('camel.Camel catalog version')).to.not.be.equal('2.22.0');
+		await checkExpectedCompletion(docUriXml, new vscode.Position(0, 21), expectedCompletion);
 		await config.update('camel.Camel catalog version', '2.22.0');
-		const actualCompletionList = await retrieveCompletionList(docUriXml, new vscode.Position(0, 21));
-		const actualCompletionLabelList = actualCompletionList.items.map(c => { return c.label; });
-		expect(actualCompletionLabelList).to.not.include('jgroups-raft:clusterName');
+
+		await checkNotExpectedCompletion(docUriXml, new vscode.Position(0, 21), expectedCompletion);
 
 		await config.update('camel.Camel catalog version', undefined);
-		await testCompletion(docUriXml, new vscode.Position(0, 21), {
-			items: expectedCompletion
-		});
+		await checkExpectedCompletion(docUriXml, new vscode.Position(0, 21), expectedCompletion);
 	});
 
 });
 
-async function testCompletion(
-	docUri: vscode.Uri,
-	position: vscode.Position,
-	expectedCompletionList: vscode.CompletionList
-) {
-	await activate(docUri);
-
-	const actualCompletionList = await retrieveCompletionList(docUri, position);
-
-	const expectedCompletionLabelList = expectedCompletionList.items.map(c => { return c.label; });
-	const actualCompletionLabelList = actualCompletionList.items.map(c => { return c.label; });
-	expect(actualCompletionLabelList).to.include.members(expectedCompletionLabelList);
-	expect(actualCompletionLabelList).to.not.include('test:name');
+export async function checkNotExpectedCompletion(docUri: vscode.Uri, position: vscode.Position, expectedCompletion: vscode.CompletionItem) {
+    let hasUnExpectedCompletion = true;
+    await waitUntil(() => {
+        // Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
+        (vscode.commands.executeCommand('vscode.executeCompletionItemProvider', docUri, position)).then(value => {
+			let actualCompletionList = value as vscode.CompletionList;
+			const completionItemFound = actualCompletionList.items.find(completion => {
+				return completion.label === expectedCompletion.label;
+			});
+            hasUnExpectedCompletion = completionItemFound !== undefined;
+        });
+        return !hasUnExpectedCompletion;
+	}, 15000, 500);
 }
-async function retrieveCompletionList(docUri: vscode.Uri, position: vscode.Position) {
-	// Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
-	return (await vscode.commands.executeCommand('vscode.executeCompletionItemProvider', docUri, position)) as vscode.CompletionList;
+
+export async function checkExpectedCompletion(docUri: vscode.Uri, position: vscode.Position, expectedCompletion: vscode.CompletionItem) {
+    let hasExpectedCompletion = false;
+    await waitUntil(() => {
+        // Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
+        (vscode.commands.executeCommand('vscode.executeCompletionItemProvider', docUri, position)).then(value => {
+			let actualCompletionList = value as vscode.CompletionList;
+			const completionItemFound = actualCompletionList.items.find(completion => {
+				return completion.label === expectedCompletion.label;
+			});
+            hasExpectedCompletion = completionItemFound !== undefined;
+        });
+        return hasExpectedCompletion;
+    }, 10000, 500);
 }
 
