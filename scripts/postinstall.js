185ff0c7cc17fe00e9a67fd923d5b004f63a34df
Aurélien Pupier <apupier@redhat.com>
2020-11-23 15:40:28 +0100

    fix postinstall script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,10 +3,9 @@
 var lsp_server_version = "1.2.0-SNAPSHOT";
 
 const download = require("mvn-artifact-download").default;
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-download('com.github.camel-tooling:camel-lsp-server:' + lsp_server_version, './jars/', 'https://oss.sonatype.org/content/repositories/snapshots/');
-//.then((filename)=>{
-	//fs.renameSync(filename, path.join('.', 'jars', 'language-server.jar'));
-//});
+download('com.github.camel-tooling:camel-lsp-server:' + lsp_server_version, './jars/', 'https://oss.sonatype.org/content/repositories/snapshots/').then((filename)=>{
+	fs.renameSync(filename, path.join('.', 'jars', 'language-server.jar'));
+});
