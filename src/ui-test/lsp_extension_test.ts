dfd5c34703250bfeff2d425367fe65c570ba91b6
Marian Lorinc <mlorinc@redhat.com>
2021-06-25 13:20:52 +0200

    FUSETOOLS2-966 - Stabilize LSP UI tests Jenkins pipeline
    Signed-off-by: Marian Lorinc <mlorinc@redhat.com>

--- src/ui-test/lsp_extension_test.ts
+++ src/ui-test/lsp_extension_test.ts
@@ -5,36 +5,31 @@ import {
 	By,
 	EditorView,
 	ExtensionsViewItem,
-	ExtensionsViewSection,
-	SideBarView,
 	until,
 	VSBrowser,
 	WebDriver
 } from 'vscode-extension-tester';
 import {
-	DefaultWait,
 	Dialog,
 	Marketplace,
-	StatusBarExt
+	StatusBarExt,
+	Workbench
 } from 'vscode-uitests-tooling';
 
 describe('Language Support for Apache Camel extension', function () {
+	this.timeout(60000);
 
 	const RESOURCES: string = path.resolve('src', 'ui-test', 'resources');
 	const CAMEL_CONTEXT_XML: string = 'camel-context.xml';
 	const LSP_STATUS_BAR_MESSAGE: string = 'Apache Camel Language Server started';
 
 	describe('Extensions view', function () {
-
 		let marketplace: Marketplace;
-		let section: ExtensionsViewSection;
 		let item: ExtensionsViewItem;
 
 		before(async function () {
-			this.timeout(10000);
-			marketplace = await Marketplace.open();
-			await DefaultWait.sleep(1000);
-			section = await new SideBarView().getContent().getSection('Installed') as ExtensionsViewSection;
+			this.retries(5);
+			marketplace = await Marketplace.open(this.timeout());
 		});
 
 		after(async function () {
@@ -43,43 +38,37 @@ describe('Language Support for Apache Camel extension', function () {
 		});
 
 		it('Find extension', async function () {
-			this.timeout(10000);
-			item = await section.findItem(`@installed ${pjson.displayName}`);
+			item = await marketplace.findExtension(`@installed ${pjson.displayName}`);
 		});
 
 		it('Extension is installed', async function () {
-			this.timeout(5000);
 			const installed = await item.isInstalled();
 			assert.isTrue(installed);
 		});
 
 		it('Verify display name', async function () {
-			this.timeout(5000);
 			const title = await item.getTitle();
 			assert.equal(title, `${pjson.displayName}`);
 		});
 
 		it('Verify description', async function () {
-			this.timeout(5000);
 			const desc = await item.getDescription();
 			assert.equal(desc, `${pjson.description}`);
 		});
 
 		it('Verify version', async function () {
-			this.timeout(5000);
 			const version = await item.getVersion();
 			assert.equal(version, `${pjson.version}`);
 		});
 	});
 
-	describe.skip('Status bar', function () {
+	describe('Status bar', function () {
 
 		let driver: WebDriver;
 
 		before(async function () {
-			this.timeout(20000);
 			driver = VSBrowser.instance.driver;
-			await Dialog.openFile(path.join(RESOURCES, CAMEL_CONTEXT_XML));
+			await new Workbench().openFile(path.join(RESOURCES, CAMEL_CONTEXT_XML), this.timeout() - 3000);
 		});
 
 		after(async function () {
@@ -87,10 +76,11 @@ describe('Language Support for Apache Camel extension', function () {
 		});
 
 		it('Language Support for Apache Camel started', async function () {
-			this.timeout(45000);
 			await driver.wait(until.elementLocated(By.id('redhat.vscode-apache-camel')), 35000);
-			const lsp = await new StatusBarExt().getLSPSupport();
-			assert.equal(lsp, LSP_STATUS_BAR_MESSAGE);
+			await driver.wait(async () => {
+				const lsp = await new StatusBarExt().getLSPSupport().catch(() => '');
+				return lsp === LSP_STATUS_BAR_MESSAGE;
+			}, this.timeout() - 3000, `Could not find Apache Camel element with label "${LSP_STATUS_BAR_MESSAGE}". Current label: "${await new StatusBarExt().getLSPSupport().catch(() => 'unknown')}"`);
 		});
 	});
 
