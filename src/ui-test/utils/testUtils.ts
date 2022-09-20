d74af0551aa677ec42961becdfe023e231240a25
Dominik Jelinek <djelinek@redhat.com>
2022-09-20 20:12:05 +0200

    FUSETOOLS-1809: Diagnostics for Camel URIs Ui tests are failing with timeout
    Signed-off-by: Dominik Jelinek <djelinek@redhat.com>

--- src/ui-test/utils/testUtils.ts
+++ src/ui-test/utils/testUtils.ts
@@ -1,4 +1,4 @@
-import { ContentAssistItem, InputBox, Workbench } from "vscode-extension-tester";
+import { ContentAssistItem, EditorView, InputBox, ModalDialog, ProblemsView, TextEditor, Workbench } from "vscode-extension-tester";
 
 export async function openFile(fileToOpenAbsolutePath?: string): Promise<void> {
 	const workbench = new Workbench();
@@ -18,4 +18,39 @@ export async function openFile(fileToOpenAbsolutePath?: string): Promise<void> {
 export async function getTextExt(item: ContentAssistItem): Promise<string> {
 	const name: string = await item.getText();
 	return name.split('\n')[0];
+}
+
+/**
+ * Workaround the issue of BottomBarPanel().openProblemsView() method
+ * For more details see https://github.com/redhat-developer/vscode-extension-tester/issues/505
+ *
+ * @param view Label of BottomBar View
+ */
+export async function openView(view: string): Promise<ProblemsView> {
+	const workbench = new Workbench();
+	await workbench.executeCommand('View: Open View');
+	await workbench.openCommandPrompt();
+	const input = await InputBox.create();
+	await input.setText('view ' + view);
+	await input.confirm();
+	return new ProblemsView();
+}
+
+/**
+ * Close editor with handling of Save/Don't Save Modal dialog
+ *
+ * @param title Title of opened active editor
+ * @param save true/false
+ */
+export async function closeEditor(title: string, save?: boolean) {
+	const dirty = await new TextEditor().isDirty();
+	await new EditorView().closeEditor(title);
+	if(dirty) {
+		const dialog = new ModalDialog();
+		if(save) {
+			await dialog.pushButton('Save');
+		} else {
+			await dialog.pushButton('Don\'t Save');
+		}
+	}
 }
