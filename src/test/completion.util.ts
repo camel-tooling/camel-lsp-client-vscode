ff1329db3894a1391540c702d117696e3a0e8e02
Aurélien Pupier <apupier@redhat.com>
2022-10-25 14:36:01 +0200

    FUSETOOLS2-1860 - improve error message on test failure
    to ease investigation on flaky tests
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/test/completion.util.ts
+++ src/test/completion.util.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 
 import { waitUntil } from 'async-wait-until';
+import { assert } from 'chai';
 
 export async function checkNotExpectedCompletion(docUri: vscode.Uri, position: vscode.Position, expectedCompletion: vscode.CompletionItem) {
     let hasUnExpectedCompletion = true;
@@ -18,16 +19,20 @@ export async function checkNotExpectedCompletion(docUri: vscode.Uri, position: v
 }
 
 export async function checkExpectedCompletion(docUri: vscode.Uri, position: vscode.Position, expectedCompletion: vscode.CompletionItem) {
-    let hasExpectedCompletion = false;
-    await waitUntil(() => {
-        // Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
-        (vscode.commands.executeCommand('vscode.executeCompletionItemProvider', docUri, position)).then(value => {
-			const actualCompletionList = value as vscode.CompletionList;
-			const completionItemFound = actualCompletionList.items.find(completion => {
-				return completion.label === expectedCompletion.label;
+	let hasExpectedCompletion = false;
+	try {
+		await waitUntil(() => {
+			// Executing the command `vscode.executeCompletionItemProvider` to simulate triggering completion
+			(vscode.commands.executeCommand('vscode.executeCompletionItemProvider', docUri, position)).then(value => {
+				const actualCompletionList = value as vscode.CompletionList;
+				const completionItemFound = actualCompletionList.items.find(completion => {
+					return completion.label === expectedCompletion.label;
+				});
+				hasExpectedCompletion = completionItemFound !== undefined;
 			});
-            hasExpectedCompletion = completionItemFound !== undefined;
-        });
-        return hasExpectedCompletion;
-    }, 10000, 500);
+			return hasExpectedCompletion;
+		}, 10000, 500);
+	} catch {
+		assert.fail(`Not found expect completion ${expectedCompletion.label}`);
+	}
 }
