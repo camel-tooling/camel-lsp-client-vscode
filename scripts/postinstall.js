82c784642e896b1dbc763077505982d93bedf8c1
Aurélien Pupier <apupier@redhat.com>
2023-10-09 15:48:21 +0200

    FUSETOOLS2-2197 - Include Camel LS Bom
    - Download sbom corresponding to embedded Camel Language Server
    - Merge it with the one of the VS Code application
    - Store it on GitHub workflow for now
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -6,6 +6,13 @@ const download = require("mvn-artifact-download").default;
 const fs = require('fs');
 const path = require('path');
 
-download('com.github.camel-tooling:camel-lsp-server:' + LSP_SERVER_VERSION, './jars/', 'https://oss.sonatype.org/content/repositories/snapshots/').then((filename)=>{
+const MAVEN_REPO_URL = 'https://oss.sonatype.org/content/repositories/snapshots/';
+
+download('com.github.camel-tooling:camel-lsp-server:' + LSP_SERVER_VERSION,
+	'./jars/', MAVEN_REPO_URL).then((filename)=>{
 	fs.renameSync(filename, path.join('.', 'jars', 'language-server.jar'));
+});
+
+download('com.github.camel-tooling:camel-lsp-server:json:cyclonedx:' + LSP_SERVER_VERSION, '.', MAVEN_REPO_URL).then((filename)=>{
+	fs.renameSync(filename, path.join('.', 'camel-ls-sbom.json'));
 });
