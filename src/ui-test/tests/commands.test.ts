f5651536500cb845cd1f192dea7cebfa964894a6
Aurélien Pupier <apupier@redhat.com>
2023-10-11 14:43:07 +0200

    Close workspace instead of folder
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- src/ui-test/tests/commands.test.ts
+++ src/ui-test/tests/commands.test.ts
@@ -155,7 +155,7 @@ describe('Create a Camel Project using command', function () {
 		await VSBrowser.instance.takeScreenshot('Screenshot with terminal maximized');
 		console.log(await new TerminalView().getText());
 		await new TerminalView().killTerminal();
-		await new Workbench().closeFolder();
+		await new Workbench().executeCommand('Workspaces: Close Workspace');
 		const currentWorkspacePath = SPECIFIC_WORKSPACE_PREFIX + currentWorkspaceIndex;
 		await driver.wait(async () => {
 			try {
