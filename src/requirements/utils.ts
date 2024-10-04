ad9c9a6d6780b29578dd6ee22f6166c44c3a55e0
Marcelo Henrique Diniz de Araujo <mdinizde@redhat.com>
2024-10-04 11:24:38 -0300

    FUSETOOLS2-2113 - Allows selection of folder when creating projects
    - Removes "enablement" for the project creation commands because we will
      be always choosing a folder work with.
    - Project creation commands do not depend on the workspace root anymore
      and can be used without one.
    - Refactor existing tests to use the additional folder selection dialog.
    - Adds a modal to confirm the project creation since the 'camel export'
      command can clean the selected output folder.
    - Opens a new VSCode instance in the newly created project's folder.
    
    Signed-off-by: Marcelo Henrique Diniz de Araujo <mdinizde@redhat.com>

--- src/requirements/utils.ts
+++ src/requirements/utils.ts
@@ -1,9 +1,44 @@
 'use strict';
 
 /* Mostly duplicated from VS Code Java */
-
+import path from 'path';
 import { workspace, WorkspaceConfiguration } from 'vscode';
 
 export function getJavaConfiguration(): WorkspaceConfiguration {
 	return workspace.getConfiguration('java');
+}
+
+/**
+ * If there are any folder in the current workspace gets the path to the first one.
+ *
+ * @returns string represent the fsPath of the first folder in the current opened workspace, undefined otherwise.
+ */
+export function getCurrentWorkingDirectory(): string | undefined {
+	const workspaceFolders = workspace.workspaceFolders;
+	if (workspaceFolders && workspaceFolders.length > 0) {
+		// Return the first workspace folder
+		return workspaceFolders[0].uri.fsPath;
+	}
+	return undefined;
+}
+
+/**
+ * Compare two given paths to see if they are equal. Normalizes the string and takes into acount case sentive OSes.
+ *
+ * @param path1 string representing the first path to be compared
+ * @param path2 string representing t,he second path to be compared
+ * @returns `true` if paths are equal `false` otherwise.
+ */
+export function arePathsEqual(path1: string, path2: string): boolean {
+	// Normalize both paths
+	const normalizedPath1 = path.normalize(path1);
+	const normalizedPath2 = path.normalize(path2);
+
+	// On Windows and macOS, perform case-insensitive comparison
+	if (process.platform === 'win32' || process.platform === 'darwin') {
+		return normalizedPath1.toLowerCase() === normalizedPath2.toLowerCase();
+	}
+
+	// On Linux (and other case-sensitive systems), compare as-is
+	return normalizedPath1 === normalizedPath2;
 }
