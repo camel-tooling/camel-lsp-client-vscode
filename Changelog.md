fa84d482118e9b43069816a8777e7f36a19a2b0e
Aurélien Pupier <apupier@redhat.com>
2025-06-23 11:42:05 +0200

    Prepare next development iteration 1.13.0-SNAPSHOT
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- Changelog.md
+++ Changelog.md
@@ -1,5 +1,7 @@
 # Change Log
 
+## 1.13.0
+
 ## 1.12.0
 
 - Remove support for Kafka connected mode completion
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const LSP_SERVER_VERSION = "1.32.0";
+const LSP_SERVER_VERSION = "1.33.0-SNAPSHOT";
 
 const download = require("mvn-artifact-download").default;
 const fs = require('fs');
@@ -8,7 +8,7 @@ const path = require('path');
 
 // Released versions: https://repo1.maven.org/maven2/
 // Snapshot versions: https://central.sonatype.com/repository/maven-snapshots/
-const MAVEN_REPO_URL = 'https://repo1.maven.org/maven2/';
+const MAVEN_REPO_URL = 'https://central.sonatype.com/repository/maven-snapshots/';
 
 download('com.github.camel-tooling:camel-lsp-server:' + LSP_SERVER_VERSION,
 	'./jars/', MAVEN_REPO_URL).then((filename)=>{
